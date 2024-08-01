934973c0c2a6307dcf333f831f80bb2e8b217686
Filip Larsson <filiplarssonnnn@gmail.com>
2024-08-01 11:30:00 +0200

    adding normalizer class

--- main.py
+++ main.py
@@ -237,6 +237,55 @@ class Visualizer():
 
 
 
+# normalizer class capable of using 2 different normalization schemes. Z-normalization as well as percentage-normalization
+class DataNormalizer():
+
+  def __init__(self):
+    # dictionary to save mean and std for each ticker for z-denormalization
+    self.tickers_mean_std = {}
+    # state to keep track of training period so we use consistent z-normalization of train and val set
+    self.trainStartDate = None
+    self.trainEndDate = None 
+  
+
+    # Function to normalize the prices using z-normalization
+  def z_normalize(self, tickername, prices, split="train"):
+      # use the mean and std of train set to normalize train and val set
+      if split == "train":
+        mean_price = np.mean(prices)
+        std_price = np.std(prices)
+        # save the mean and std for the ticker
+        self.tickers_mean_std[tickername] = (mean_price, std_price)
+      elif split == "val":
+        # we use the training period to normalize the val set's prices (for example if we trained on 2010-2015 data 
+        # we use this period's mean and std when normalizing the val ticker's prices)
+        if self.trainStartDate is None or self.trainEndDate is None:
+          raise ValueError("Training period not set. Cannot normalize val data.")
+        if tickername not in self.tickers_mean_std:
+          data = yf.download(tickername, self.trainStartDate, self.trainEndDate)
+          prices_for_mean_and_std = data['Close'].values
+          mean_price = np.mean(prices_for_mean_and_std)
+          std_price = np.std(prices_for_mean_and_std)
+          # save the mean and std for the ticker
+          self.tickers_mean_std[tickername] = (mean_price, std_price)
+        else:
+          mean_price, std_price = self.tickers_mean_std[tickername]
+
+      normalized_prices = (prices - mean_price) / std_price
+      return normalized_prices
+
+
+  # Function to reverse the z-normalization
+  def z_denormalize(self, tickername, normalized_prices):
+      if tickername not in self.tickers_mean_std:
+          raise ValueError(f"Statistics for ticker {tickername} not found.")
+      mean_price, std_price = self.tickers_mean_std[tickername]
+      original_prices = (normalized_prices * std_price) + mean_price
+      return original_prices
+    
+
+
+
 # dataloader class
 class DataLoader():
 
@@ -597,33 +646,35 @@ grad_norms = []
 # load the data into our program
 dataLoader = DataLoader(model_config)
 
-# training specification
-trainTickerList = ['SMCI']
+# Training specification
+trainTickerList = ['SMCI', 'AMZN', 'GME', 'CHWY', 'FFIE', 'AAPL', 'MSFT', 'GOOGL', 'TSLA', 'META',
+                   'NFLX', 'ADBE', 'NVDA', 'AMD', 'BABA', 'INTC', 'PYPL', 'CRM', 'QCOM', 'ORCL']
 train_data_file = "train_data.txt"
-train_startDate = '2019-08-01'
-train_endDate = '2024-08-01'
+train_startDate = '2012-08-01'
+train_endDate = '2022-08-01'
 
-# load the train set into dataloader
+# Load the train set into dataloader
 dataLoader.load_data_from_yfinance(trainTickerList, train_data_file, startDate=train_startDate, endDate=train_endDate, split="train")
 
 
-# validation specification
-valTickerList = ['NVDA']
+# Validation specification
+valTickerList = ['NVDA', 'AMD', 'TSLA', 'GOOGL', 'AAPL']
 val_data_file = "val_data.txt"
-val_startDate = '2023-08-01'
+val_startDate = '2022-08-02'
 val_endDate = '2024-07-01'
 
-# load the val set into dataloader
+# Load the validation set into dataloader
 dataLoader.load_data_from_yfinance(valTickerList, val_data_file, startDate=val_startDate, endDate=val_endDate, split="val")
+
 
 # how many batches between each val loss measurement
 val_interval = 50
 # record lowest val loss
-lowest_val_loss = None
+lowest_val_loss = float('inf')
 
 print(f"dataloader.dataPerEpoch: {dataLoader.dataPerEpoch}")
 
-epochs = 200
+epochs = 6
 
 batch_size = dataLoader.B * dataLoader.T
 
@@ -710,6 +761,13 @@ with tqdm(total=(total_batches), desc=f"Training progress") as pbar:
             tickerPreds = tickerPreds.view(-1).cpu().numpy()
             tickerTargets = tickerTargets.view(-1).cpu().numpy()
 
+            deNormalize_prices = True
+            if deNormalize_prices:
+              # call the dataloaders denormalize() function to reverse normalization for model preds and targets of current ticker
+              current_ticker = valTickerList[tickerIndex]
+              tickerPreds = dataLoader.denormalize(current_ticker, tickerPreds)
+              tickerTargets = dataLoader.denormalize(current_ticker, tickerTargets)
+
             vliser.plot_preds(actual_prices=tickerTargets, preds=tickerPreds, title=f"Val Ticker: {valTickerList[tickerIndex]}, period: {val_startDate} to {val_endDate}, processed {int((i / total_batches)*100)}% of training data", width=16)
 
         # calculate average loss over tickers
@@ -717,7 +775,7 @@ with tqdm(total=(total_batches), desc=f"Training progress") as pbar:
         # append the test loss
         val_losses.append(val_loss.item())
         # check if this is lowest loss
-        if lowest_val_loss is None or val_loss.item() < lowest_val_loss:
+        if val_loss.item() < lowest_val_loss:
           lowest_val_loss = val_loss
 
 
